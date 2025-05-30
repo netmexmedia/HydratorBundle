8c437a448c00288f25ae7f4856392c7adbf0792a
Danny Verhoeven <d.verhoeven95@live.nl>
2025-05-30 20:30:04 +0200

    Test custom config

--- src/HydratorBundle.php
+++ src/HydratorBundle.php
@@ -8,8 +8,22 @@ use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class HydratorBundle extends AbstractBundle
 {
-    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    public function loadExtension(array $configs, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
-        $container->import(__DIR__ . '/../config/services.yaml');
+        $processor = new Processor();
+        $configuration = new Configuration();
+        $config = $processor->processConfiguration($configuration, $configs);
+
+        $transformersPath = $config['transformers_path'];
+        $transformerTag = $config['transformer_tag'];
+
+        $builder->setParameter('netmex_hydrator.transformers_path', $transformersPath);
+        $builder->setParameter('netmex_hydrator.transformer_tag', $transformerTag);
+
+        $container->services()
+            ->load('App\\Transformer\\', $transformersPath)
+            ->autowire()
+            ->autoconfigure()
+            ->tag($transformerTag);
     }
 }
